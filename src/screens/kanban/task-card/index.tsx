d5f91543e07861b08cfc2ec12d1ee059c87861c0
boyang <1028524431@qq.com>
2021-05-09 21:02:26 +0800

    feat: 标题高亮

--- src/screens/kanban/task-card/index.tsx
+++ src/screens/kanban/task-card/index.tsx
@@ -2,7 +2,9 @@ import { Card } from "antd";
 import AlertModel from "models/alert";
 import { ITask } from "models/task";
 import { FC } from "react";
+import useUrlParams from "utils/use-url-params";
 import XTaskIcon from "./task-icon";
+import XTaskTitle from "./task-title";
 
 interface IXTaskCard {
     info: ITask
@@ -10,12 +12,14 @@ interface IXTaskCard {
 }
 
 const XTaskCard: FC<IXTaskCard> = ({info, className}) => {
+
+    const [param, setParams] = useUrlParams(['name'])
     const editor = () => {
         AlertModel.taskForm({taskId: info.id})
     }
     
     return <Card className={className} onClick={editor} >
-        <p>{info.name}</p>
+        <XTaskTitle txt={info.name} keyword ={param.name} />
         <XTaskIcon info={info} />
     </Card>
 }
