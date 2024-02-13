02b4ecf7016231e9961426cb5eee0ce80b4c1d80
lby <lby@123>
2024-02-13 11:43:01 +0800

    v3:init

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,9 @@
-import { useUser } from 'context/user-cotext';
-import XLogout from 'logout';
-import XRouter from 'router';
 
 function App() {
-  const {user} = useUser()
 
   return (
     <div className="App">
-      { user ? <XRouter /> : <XLogout /> }
+      app
     </div>
   );
 }
